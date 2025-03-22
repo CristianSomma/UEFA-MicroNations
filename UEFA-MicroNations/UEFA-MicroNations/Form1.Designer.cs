82a0580cc49449d95bd424fd540a7d42d5081c19
Cristian Somma <12999453@itisrossi.vi.it>
2025-03-22 09:46:13 +0100

    Update

---
+++ UEFA-MicroNations/UEFA-MicroNations/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace UEFA_MicroNations
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTitleP2 = new System.Windows.Forms.Label();
+            this.lblUEFATitle = new System.Windows.Forms.Label();
+            this.lstWindow = new System.Windows.Forms.ListView();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(163)))), ((int)(((byte)(55)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(934, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 711);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(163)))), ((int)(((byte)(55)))));
+            this.panel2.Controls.Add(this.lblTitleP2);
+            this.panel2.Controls.Add(this.lblUEFATitle);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(934, 75);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblTitleP2
+            // 
+            this.lblTitleP2.AutoSize = true;
+            this.lblTitleP2.Font = new System.Drawing.Font("Corbel", 30F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleP2.ForeColor = System.Drawing.Color.White;
+            this.lblTitleP2.Location = new System.Drawing.Point(136, 14);
+            this.lblTitleP2.Name = "lblTitleP2";
+            this.lblTitleP2.Size = new System.Drawing.Size(240, 49);
+            this.lblTitleP2.TabIndex = 1;
+            this.lblTitleP2.Text = "MicroNations";
+            // 
+            // lblUEFATitle
+            // 
+            this.lblUEFATitle.AutoSize = true;
+            this.lblUEFATitle.Font = new System.Drawing.Font("Corbel", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUEFATitle.ForeColor = System.Drawing.Color.White;
+            this.lblUEFATitle.Location = new System.Drawing.Point(12, 14);
+            this.lblUEFATitle.Name = "lblUEFATitle";
+            this.lblUEFATitle.Size = new System.Drawing.Size(118, 49);
+            this.lblUEFATitle.TabIndex = 0;
+            this.lblUEFATitle.Text = "UEFA";
+            // 
+            // lstWindow
+            // 
+            this.lstWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstWindow.Font = new System.Drawing.Font("Noto Mono", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstWindow.HideSelection = false;
+            this.lstWindow.Location = new System.Drawing.Point(0, 75);
+            this.lstWindow.Name = "lstWindow";
+            this.lstWindow.Size = new System.Drawing.Size(934, 636);
+            this.lstWindow.TabIndex = 2;
+            this.lstWindow.UseCompatibleStateImageBehavior = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 711);
+            this.Controls.Add(this.lstWindow);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "UEFA MicroNations";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTitleP2;
+        private System.Windows.Forms.Label lblUEFATitle;
+        private System.Windows.Forms.ListView lstWindow;
+    }
+}
+
---
+++ UEFA-MicroNations/UEFA-MicroNations/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UEFA_MicroNations
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            RetrieveFromDatabase();
+        }
+
+        private void RetrieveFromDatabase()
+        {
+            using(StreamReader streamReader = new StreamReader("../../files/database.csv"))
+            {
+                
+                int index = 0;
+                while (!streamReader.EndOfStream)
+                {
+                    string record = streamReader.ReadLine();
+                    string[] recordFields = record.Split(';');
+
+                    if (index == 0)
+                    {
+                        
+                        index++;
+                        continue;
+                    }
+
+                    
+                    Match oldMatch = new Match(
+                        recordFields[0],
+                        recordFields[1],
+                        int.Parse(recordFields[2]),
+                        int.Parse(recordFields[3]));
+
+                    AddMatchInList(oldMatch);
+                }
+            }
+        }
+
+        private void CheckDatabaseFormatting(string[] titleRecord)
+        {
+            string[] englishFieldsNames = new string[]
+                {
+                    "hometeam",
+                    "awayteam",
+                    "homegoals",
+                    "awaygoals"
+                };
+
+            string[] italianFieldsNames = new string[]
+            {
+                    "teamcasa",
+                    "teamospite",
+                    "casagoals",
+                    "ospitegoals"
+            };
+
+            if (englishFieldsNames.Contains(titleRecord[0].ToLower())){
+                Array.IndexOf(englishFieldsNames, titleRecord[0].ToLower())
+            }
+                
+        }
+
+        private void AddMatchInList(Match matchToAdd)
+        {
+            string newLine = $"{matchToAdd.HomeTeam.PadRight(35)}" +
+                $"{matchToAdd.HomeGoals.ToString().PadRight(5)}:" +
+                $"{matchToAdd.AwayGoals.ToString().PadLeft(5)}" +
+                $"{matchToAdd.AwayTeam.PadLeft(35)}";
+
+            lstWindow.Items.Add(newLine);
+        }
+    }
+
+    public struct Match
+    {
+        public string HomeTeam, AwayTeam;
+        public int HomeGoals, AwayGoals;
+
+        public Match(string homeTeam, string awayTeam, int homeGoals, int awayGoals)
+        {
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+            HomeGoals = homeGoals;
+            AwayGoals = awayGoals;
+        }
+    }
+}
